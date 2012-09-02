ba40e29e7aeea57f35c7188088e94f86183a5b6a
Christian DeHoyos <christian@uservoice.com>
2012-09-01 23:04:19 -0700

    First surf project commit -cd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurfX::Application.config.secret_token = '345a67beb834e452af359a7540f52c6639920a09c55b02cb390e0f4163672454d093871dbec80fb3ddbf77d89198c327d843c26e0334120a725dc1cdae4d3a37'
